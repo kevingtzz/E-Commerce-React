0fcb5df384d1f38acf6d41ac183f8faa54fc04b6
kevingtzz <kevingtzz@outlook.com>
2022-12-15 17:47:08 -0500

    feat(App): Persisteng Navigation test component add with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+
+const Navigation = () => {
+  return (
+    <>
+      <div className=''>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };

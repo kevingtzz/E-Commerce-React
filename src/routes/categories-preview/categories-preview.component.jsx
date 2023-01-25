249ada04b26f415dba2d2809384d489974617edc
kevingtzz <kevingtzz@outlook.com>
2023-01-24 19:19:02 -0500

    refactor(categories): context to Redux migration

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useSelector } from "react-redux";
 
-import { CategoriesContext } from "../../constexts/categories.context";
 import CategoryPreview from "../../components/category-preview/cartegory-preview.component";
 
+import { selectCategoriesMap } from "../../store/categories/category.selector";
+
 const CategoriesPreview = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
 
   return (
     <>

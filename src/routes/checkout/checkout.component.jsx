e0e77422e962b6ab97915170e75a8812c12c8855
kevingtzz <kevingtzz@outlook.com>
2023-03-12 21:50:58 -0500

    feat: stripe functionalities add

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 
@@ -38,6 +39,7 @@ const Checkout = () => {
         cartItems.map(cartItem => (<CheckoutItem key={cartItem.id} cartItem={cartItem} />))
       }
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };

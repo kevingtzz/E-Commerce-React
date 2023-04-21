ea75938d40a11d14343c153d35c715dddc4059ca
kevingtzz <kevingtzz@outlook.com>
2023-04-21 00:01:15 -0500

    ref(cart store): migration to typeScript

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector.js';
-import { setIsCartOpen } from '../../store/cart/cart.action.js';
+import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector';
+import { setIsCartOpen } from '../../store/cart/cart.action';
 
 import {
   ShoppingIcon,

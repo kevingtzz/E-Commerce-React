67e66de1fd9e1573a44bebf23196ece3613b1f83
Kevin Gutierrez <kevingtzz@outlook.com>
2023-08-16 23:20:50 -0500

    feat(App): dynamic home import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useEffect, lazy, suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
+// import Home from "./routes/home/home.component"; // Static import
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
+import Spinner from "./components/spinner/spinner.component";
+import Checkout from "./routes/checkout/checkout.component";
 
-import { checkUserSession } from './store/user/user.action';
+import { checkUserSession } from "./store/user/user.action";
+
+const Home = lazy(() => import("./routes/home/home.component")); // Dymamic import
 
 const App = () => {
   const dispatch = useDispatch();
@@ -19,14 +22,16 @@ const App = () => {
   }, []); // dispatch dependency is missing but, dispach is a hook, so it never changes, you can choose if you want to put that, but it's not necessary.
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 

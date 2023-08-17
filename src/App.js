12b3207152b1602953e7e5040938590ef1de8ae8
Kevin Gutierrez <kevingtzz@outlook.com>
2023-08-16 23:25:43 -0500

    feat(App): dynamic import

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 // import Home from "./routes/home/home.component"; // Static import
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Spinner from "./components/spinner/spinner.component";
-import Checkout from "./routes/checkout/checkout.component";
+// import Navigation from "./routes/navigation/navigation.component";
+// import Authentication from "./routes/authentication/authentication.component";
+// import Shop from "./routes/shop/shop.component";
+// import Spinner from "./components/spinner/spinner.component";
+// import Checkout from "./routes/checkout/checkout.component";
 
 import { checkUserSession } from "./store/user/user.action";
 
 const Home = lazy(() => import("./routes/home/home.component")); // Dymamic import
+const Navigation = lazy(() =>
+  import("./routes/navigation/navigation.component")
+);
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication.component")
+);
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
   const dispatch = useDispatch();
